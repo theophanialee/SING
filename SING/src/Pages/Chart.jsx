017464091a21ec6f117dc92ae1ae29740c5a2267
Theophania <pha.randomacc@gmail.com>
2023-06-10 17:11:36 +0800

    Update Chart.jsx
    Test API

--- SING/src/Pages/Chart.jsx
+++ SING/src/Pages/Chart.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function Chart() {
   const [chart, setChart] = useState([]);
@@ -10,7 +10,7 @@ export default function Chart() {
       );
       const chartData = await response.json();
       console.log(chartData);
-      setChart(chartData.body.track_list);
+      setChart(chartData.message.body.track_list);
     }
     getChart();
   }, []);
@@ -21,21 +21,24 @@ export default function Chart() {
       <h1>SG TOP 50</h1>
       <table>
         <caption>Chart 50</caption>
-        <tr>
-          <th>No.</th>
-          <th></th>
-          <th>Track</th>
-          <th>Artist</th>
-        </tr>
-
-        {chart.map((item, id) => (
-          <tr key={id}>
-            <th>{id + 1}</th>
-            <th>img</th>
-            <th>{item.track.track_name}</th>
-            <th>{item.track.artist_name}</th>
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th></th>
+            <th>Track</th>
+            <th>Artist</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {chart.map((item, id) => (
+            <tr key={id}>
+              <th>{id + 1}</th>
+              <th>img</th>
+              <th>{item.track.track_name}</th>
+              <th>{item.track.artist_name}</th>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </>
   );
